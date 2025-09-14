be9b11c5abe49368edd9573b13f770f941448f34
hassannahid105 <infosharemail405@gmail.com>
2025-09-14 13:09:53 +0600

    about page our vision section design

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,9 +1,11 @@
 import OurHistory from "./OurHistory/OurHistory";
+import OurVision from "./OUrVision/OUrVision";
 
 const AboutUs = () => {
   return (
     <div className="w-10/12 mx-auto">
       <OurHistory></OurHistory>
+      <OurVision></OurVision>
     </div>
   );
 };
--- src/pages/AboutUs/OUrVision/OUrVision.jsx
+++ src/pages/AboutUs/OUrVision/OUrVision.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import historyImg from "../../../assets/AboutUs/about_us_img_21-1.png";
+const OurVision = () => {
+  return (
+    <section className="grid grid-cols-4 section gap-12 items-center justify-center">
+      <div className="col-span-2">
+        <img src={historyImg} alt="" />
+      </div>
+      <div className="col-span-2 w-10/12 py-10">
+        <p className="text-skydeep font-medium py-4">Our vision</p>
+        <h3 className="text-4xl font-bold my-6">We are marketpress</h3>
+        <p className="opacity-70 my-4">
+          Dynamically procrastinate B2C users after installed base benefits.
+          Dramatically visualize customer directed convergence without
+          revolutionary ROI
+        </p>
 
-const OUrVision = () => {
-  return <div></div>;
+        <div className="flex flex-col gap-2 mt-6 text-sm font-semibold">
+          <p className="">Credibly innovate granular internal</p>
+          <p className="">Grantedly underconstructions reloaded</p>
+          <p className="">Interactively procrastinate high-payoff</p>
+          <p className="">Completely synergize resource taxing relationships</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default OUrVision;
+export default OurVision;
